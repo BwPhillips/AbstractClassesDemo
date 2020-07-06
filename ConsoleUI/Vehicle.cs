ec3af6b934f16588700341eb3947bb47b6ad624e
Brenton Phillips <phillips.brenton@gmail.com>
2020-07-06 16:08:50 -0500

    Abstract Classes Completed

---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ConsoleUI
+{
+    public abstract class Vehicle
+    {
+        public string Year { get; set; } = "Default Year";
+        public string Make { get; set; } = "Default Make";
+        public string Model { get; set; } = "Default Model";
+
+        public abstract void DriveAbstracts();
+
+        public virtual void DriveVirtual()
+        {
+            Console.WriteLine($"This {GetType().Name} is virtually in drive.");
+        }
+    }
+}
